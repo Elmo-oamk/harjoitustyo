a5d8f16ffa96dd65b4d08cfd9a6778bc9cbc6c23
Elmo Lehtosaari <elehtosa24@students.oamk.fi>
2025-07-28 17:25:07 +0300

    Update db.js

--- Harjoitustyö/Opintorekisteri-api/db.js
+++ Harjoitustyö/Opintorekisteri-api/db.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'root',          // vaihda tarvittaessa
-  password: '123456',  // vaihda tarvittaessa
+  user: 'root',          
+  password: '123456',  
   database: 'Opintorekisteri'
 });
 
